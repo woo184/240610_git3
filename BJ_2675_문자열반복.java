75a276bbcc8ff5b97cb019af7dc8b7ccebdea683
woo184 <kthl2@naver.com>
2024-06-17 00:28:51 +0900

    add commit

--- BJ_2675_문자열반복.java
+++ BJ_2675_문자열반복.java
@@ -11,22 +11,22 @@ public class BJ_2675_문자열반복 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine()); // 반복횟수 결정
 		
 		
 		for(int i=0; i<T; i++) {
 			
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine()); 
 			
-			int R = Integer.parseInt(st.nextToken());
-			String str = st.nextToken();
+			int R = Integer.parseInt(st.nextToken());  // 정수값 띄어쓰기 위해 Tokenizer 적용
+			String str = st.nextToken(); // 문자열 띄어쓰기 위해 Tokenizer 적용
 			
-			for(int j =0; j<str.length(); j++) {
-				for(int k=0; k<R; k++) {
-					System.out.print(str.charAt(j));
+			for(int j =0; j<str.length(); j++) {   // 문자열의 길이만큼 반복
+				for(int k=0; k<R; k++) { // 정수값 입력받은 수 만큼 반복 
+					System.out.print(str.charAt(j)); // 1번째 반복 3입력 str입력값의 charAt(0)번째값 A를 세번출력 // 두번째 반복 j는 1 charAt(1) 012의 두번째 인자값 B를 세번출력 계속해서 반복
 				}
 			}
-			System.out.println("");
+			System.out.println(""); // 띄워쓰기 위해 사용
 		}
 		
 	}
